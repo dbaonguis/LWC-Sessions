52aa99803fd19b63f9be27b9c409df1e107448d3
Donato Baonguis <dbaonguis@yahoo.com>
2019-08-14 19:40:02 -0400

    Add files via upload

---
+++ force-app/main/default/lwc/publicMethodParent/publicMethodParent.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+import { LightningElement, track } from 'lwc';
+
+export default class PublicMethodParent extends LightningElement {
+    @track
+    value;
+
+    checkboxSelectHandler() {
+        const childComponent = this.template.querySelector('c-public-method-child');
+        const returnedMessage = childComponent.selectCheckbox(this.value);
+        console.log('Returned Message: ', returnedMessage);
+    }
+
+    onInputChangeHandler(event) {
+        this.value = event.target.value;
+    }
+}
