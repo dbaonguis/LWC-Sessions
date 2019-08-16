5fde6f4866e8cb02b2cb44f64f322391ea2a7ca9
Donato Baonguis <dbaonguis@yahoo.com>
2019-08-16 18:23:52 -0400

    Add files via upload

--- force-app/main/default/lwc/meetingRoom/meetingRoom.js
+++ force-app/main/default/lwc/meetingRoom/meetingRoom.js
@@ -1,10 +1,14 @@
 import { LightningElement, api } from 'lwc';
 
 export default class MeetingRoom extends LightningElement {
-    //@api
-    //meetingRoomInfo = {roomName:'A-01', roomCapacity:'12'};
+    @api
+    meetingRoomInfo = {roomName:'A-01', roomCapacity:'12'};
 
     @api
     showRoomInfo = false;
-    
+
+    tileClickHandler() {
+        const tileClicked = new CustomEvent('tileclick', {detail: this.meetingRoomInfo, bubbles: true});
+        this.dispatchEvent(tileClicked);
+    }
 }
