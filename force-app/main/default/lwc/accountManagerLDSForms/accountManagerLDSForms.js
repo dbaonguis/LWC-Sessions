4c14ab049ac21e7824031e4bcb305d0768472a1a
Donato Baonguis <dbaonguis@yahoo.com>
2019-08-23 17:21:34 -0400

    Add files via upload

---
+++ force-app/main/default/lwc/accountManagerLDSForms/accountManagerLDSForms.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+
+import { LightningElement, track } from 'lwc';
+
+export default class AccountManagerLDSForms extends LightningElement {
+    @track
+    recordId;
+    
+    successHandler(event) {
+        this.recordId = event.detail.id;
+    }
+}
